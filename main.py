441597e9c633ec9220bf261a1ab3ba028f7bdf77
Степан <stepantishhen@mail.ru>
2021-11-14 19:11:35 +0300

    big_update2

--- main.py
+++ main.py
@@ -232,6 +232,6 @@ def clear_forms():
 
 if __name__ == '__main__':
     db_session.global_init('db/tickets.sqlite')
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
